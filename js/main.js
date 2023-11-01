e0008922c56c0e1bcb4d4ecc76e6098ff7d4af91
Alfonso Marquez <marquez.amlm@gmail.com>
2023-11-01 17:22:05 +0100

    start screen (index.html) and game over screen (gameover.html) are now usable, play.html is now the game screen

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const player = new Player();
 const cookieArr = [];
 const raindropArr = [];
 const poopArr = [];
+const heroArr = [];
 
 document.addEventListener("keydown", (e) => {
     switch (e.code) {
@@ -25,6 +26,10 @@ function spawnRaindrop() {
 function spawnPoop() {
     const addPoop = new Poop();
     poopArr.push(addPoop);
+}
+function spawnHero() {
+    const addHero = new Hero();
+    heroArr.push(addHero);
 }
 
 function despawnCookie() {
@@ -84,16 +89,36 @@ function despawnPoop() {
     })
 }
 
+function despawnHero() {
+    heroArr.forEach((addedSprite) => {
+        addedSprite.fallDown();
+
+        const collision = player.positionX < addedSprite.positionX + addedSprite.width && player.positionX + player.width > addedSprite.positionX && player.positionY < addedSprite.positionY + addedSprite.height && player.positionY + player.height > addedSprite.positionY;
+
+        if (collision) {
+            addedSprite.sprite.remove();
+            heroArr.splice(heroArr.length);
+            console.log("Oh Yeah!")
+        }
+
+        if (addedSprite.positionY === 5) {
+            addedSprite.sprite.remove();
+            heroArr.splice(heroArr.length);
+        }
+    })
+}
+
 // control sprite spawn rate and frequency
-const helloCookie = setInterval(spawnCookie, 1500);
-const helloRaindrop = setInterval(spawnRaindrop, 1500);
-const helloPoop = setInterval(spawnPoop, 1500);
+const generateCookie = setInterval(spawnCookie, 3 * 1000);
+const generateRaindrop = setInterval(spawnRaindrop, 1000);
+const generatePoop = setInterval(spawnPoop, 5 * 1000);
+const generateHero = setInterval(spawnHero, 20 * 1000);
 
 // control sprite fall speed
-const goodbyeCookie = setInterval(despawnCookie, 50);
-const goodbyeRaindrop = setInterval(despawnRaindrop, 50);
-const goodbyePoop = setInterval(despawnPoop, 50);
-
+const speedCookie = setInterval(despawnCookie, 50);
+const speedRaindrop = setInterval(despawnRaindrop, 35);
+const speedPoop = setInterval(despawnPoop, 20);
+const speedHero = setInterval(despawnHero, 25);
 
 
 //const scoreboard = document.querySelector('#scoreboard');
--- js/objects-main.js
+++ js/objects-main.js
@@ -1,6 +1,6 @@
 class Player {
     constructor() {
-        this.width = 10;
+        this.width = 5;
         this.height = 10;
         this.positionX = 43 - (this.width / 2);
         this.positionY = 5;
@@ -12,14 +12,14 @@ class Player {
         this.playerElement.style.left = this.positionX + "vw";
         this.playerElement.style.bottom = this.positionY + "vh";
 
-        this.playerElement.style.backgroundImage = "url('./images/game.sprite.herocookie.nobg.png')";
+        this.playerElement.style.backgroundImage = "url('./images/game.sprite.player.right.nobg.png')";
     }
     moveLeft() {
-        this.positionX -= 2.5;
+        this.positionX -= 3;
         this.playerElement.style.left = this.positionX + "vw";
     }
     moveRight() {
-        this.positionX += 2.5;
+        this.positionX += 3;
         this.playerElement.style.left = this.positionX + "vw";
     }
 }
@@ -94,8 +94,8 @@ class Raindrop extends Sprite {
 class Poop extends Sprite {
     constructor() {
         super();
-        this.width = 5;
-        this.height = 10;
+        this.width = 8;
+        this.height = 20;
         this.createSprite();
         this.sprite.classList.add("poop");
 
@@ -104,6 +104,25 @@ class Poop extends Sprite {
             this.sprite.style.height = this.height + "vh";
 
             this.sprite.style.backgroundImage = "url('./images/game.sprite.poop.nobg.png')";
+        }
+
+        this.fallDown();
+    }
+}
+
+class Hero extends Sprite {
+    constructor() {
+        super();
+        this.width = 10;
+        this.height = 10;
+        this.createSprite();
+        this.sprite.classList.add("hero");
+
+        if (this.sprite) {
+            this.sprite.style.width = this.width + "vw";
+            this.sprite.style.height = this.height + "vh";
+
+            this.sprite.style.backgroundImage = "url('./images/game.sprite.herocookie.nobg.png')";
         }
 
         this.fallDown();
