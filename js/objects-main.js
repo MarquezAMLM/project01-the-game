fd96dde91bd3ebb1c8bfa94d2f2a9fcf1723977c
Alfonso Marquez <marquez.amlm@gmail.com>
2023-11-01 13:46:48 +0100

    restructuring and relocating game objects and JS documents

--- js/objects-main.js
+++ js/objects-main.js
@@ -1,3 +1,29 @@
+class Player {
+    constructor() {
+        this.width = 10;
+        this.height = 10;
+        this.positionX = 43 - (this.width / 2);
+        this.positionY = 5;
+
+        this.playerElement = document.getElementById("player");
+
+        this.playerElement.style.width = this.width + "vw";
+        this.playerElement.style.height = this.height + "vh";
+        this.playerElement.style.left = this.positionX + "vw";
+        this.playerElement.style.bottom = this.positionY + "vh";
+
+        this.playerElement.style.backgroundImage = "url('./images/game.sprite.herocookie.nobg.png')";
+    }
+    moveLeft() {
+        this.positionX -= 2.5;
+        this.playerElement.style.left = this.positionX + "vw";
+    }
+    moveRight() {
+        this.positionX += 2.5;
+        this.playerElement.style.left = this.positionX + "vw";
+    }
+}
+
 class Sprite {
     constructor() {
         this.sprite = null;
